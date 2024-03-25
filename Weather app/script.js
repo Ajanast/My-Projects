f858ce5fe08d45aafc0713c9fdc6ad322fd94e6e
Anastraj <142212897+Ajanast@users.noreply.github.com>
2024-03-25 01:08:35 -0700

    Update script.js

--- Weather app/script.js
+++ Weather app/script.js
@@ -1,34 +1,31 @@
-document.addEventListener("DOMContentLoaded",function(){
-    const apiurl="https://api.openweathermap.org/data/2.5/weather?q=";
+document.addEventListener("DOMContentLoaded", function() {
+    const apiurl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    const apiid = "1846c734b37afc0db9c8f998114f7ef0";
+    const searchinput = document.getElementById("searchinp");
+    const searchbtn = document.getElementById("searchbutton");
 
-//`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
-const apiid="1846c734b37afc0db9c8f998114f7ef0";
-const searchinput=document.getElementById("searchinp")
-const searchbtn=document.getElementById("searchbutton")
-async function getweather(){
-    await weather(searchinput.value)
-}
-async function weather(city){
-    try{
-        const response=await fetch(`${apiurl}${city}&units=metric&apiid=${apiid}`);
-    const data=await response.json()
-    if(data.code===401){
-        console.error(data.message)
+    async function getweather() {
+        await weather(searchinput.value);
     }
-    if(data.main && data.main.temp!==undefined){
-        console.log(data)
-    document.getElementById("city-info").innerHTML=data.name;
-    document.getElementById("city-data").innerHTML=Math.round(data.main.temp)+"°C";
 
-    }else{
-        console.error("Weather data not available for the specified city")
+    async function weather(city) {
+        try {
+            const response = await fetch(`${apiurl}${city}&units=metric&appid=${apiid}`);
+            const data = await response.json();
+            if (data.cod === 401) {
+                console.error(data.message);
+            }
+            if (data.main && data.main.temp !== undefined) {
+                console.log(data);
+                document.getElementById("city-info").innerHTML = data.name;
+                document.getElementById("city-data").innerHTML = Math.round(data.main.temp) + "°C";
+            } else {
+                console.error("Weather data not available for the specified city");        
+            }
+        } catch (error) {
+            console.error("Error: ", error);
+        }
     }
-    
-    }
-    catch(error){
-        console.error("error : ",error)
-    }
-}
-searchbtn.addEventListener("click",getweather);
 
+    searchbtn.addEventListener("click", getweather);
 });
